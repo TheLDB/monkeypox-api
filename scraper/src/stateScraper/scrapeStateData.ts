237f32975f9a1dd3270770e7368616cd4a34dd2e
TheLDB <ldb@erikboles.com>
2022-08-05 14:48:56 -0600

    removed unnecessary variables

--- scraper/src/stateScraper/scrapeStateData.ts
+++ scraper/src/stateScraper/scrapeStateData.ts
@@ -15,12 +15,12 @@ const scrapeStateData = async () => {
 	await page.goto("https://www.cdc.gov/poxvirus/monkeypox/response/2022/us-map.html");
 
     // * Create a Chromium DevTools instance and modify download behavior to download and use data CSV
-    const setDownloadSettings = page.target().createCDPSession().then(client => {
+    await page.target().createCDPSession().then(client => {
         return client.send('Page.setDownloadBehavior', {behavior: 'allow', downloadPath: './data'})
     });
 
     // * Click download button and wait for it to download
-    const link = await page.click('[download="2022 U.S. Map & Case Count.csv"]');
+    await page.click('[download="2022 U.S. Map & Case Count.csv"]');
     await page.waitForTimeout(2000);
 
     // * Create a read stream on our new file and use @fast-csv/parse to parse it into usable, readable data
