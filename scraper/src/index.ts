5f84cbb5062e59c526d145f0e2d21b437f94121d
TheLDB <ldb@erikboles.com>
2022-08-06 13:28:57 -0600

    updated scraper function

--- scraper/src/index.ts
+++ scraper/src/index.ts
@@ -1,6 +1,9 @@
 import { scrapeStateData } from './stateScraper/scrapeStateData';
 import { scrapeCountryData } from './countryScraper/scrapeCountryData';
 
-// * Scrape all of US & indivual state data (thanks cdc)
-// scrapeStateData();
-scrapeCountryData();
+const scrapeData = async () => {
+    await scrapeStateData();
+    await scrapeCountryData();
+}
+
+scrapeData();
